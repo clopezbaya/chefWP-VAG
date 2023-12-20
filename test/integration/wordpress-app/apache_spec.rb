74b5b9f1d2e4642f664aab6ce81af2b765b5af9f
Christian Lopez <Christian.Lopez@fundacion-jala.org>
2023-12-20 08:33:36 -0400

    add new test for PHP, MYSQL, WORDPRESS

--- test/integration/wordpress-app/apache_spec.rb
+++ test/integration/wordpress-app/apache_spec.rb
@@ -1,7 +1,9 @@
+# Verificar si Apache2 esta instalado
 describe package('apache2') do
   it { should be_installed }
 end
 
+# Verificar si Apache2 esta instalado, habilitado y corriendo
 describe service('apache2') do
   it { should be_installed }
   it { should be_enabled }
